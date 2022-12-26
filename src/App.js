0ced4731fe2011e0703037f993560275b5ce1de3
James Rozsypal <jimmerxl@gmail.com>
2022-12-26 14:14:32 -0500

    Make App.js a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import './App.css';
+import Footer from './components/Footer';
+import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div className='App'>
         <Header />
+        <Footer />
       </div>
     );
   }
