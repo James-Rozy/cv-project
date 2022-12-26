b93ee78d861796d2ed1aa2fb669dde178bc17c50
James Rozsypal <jimmerxl@gmail.com>
2022-12-26 15:30:35 -0500

    Add class component structure

---
+++ src/components/CVContent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class CVContent extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <main>
+        <h2>CV Content</h2>
+      </main>
+    );
+  }
+}
+
+export default CVContent;
