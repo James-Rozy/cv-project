ac25495951340803be54e1713dd747cce26ae5db
James Rozsypal <jimmerxl@gmail.com>
2022-12-26 15:28:17 -0500

    Add CV to the beginning of the file name

---
+++ src/components/CVEducation.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CVEducation extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default CVEducation;
