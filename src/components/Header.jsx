1aa860d196ca7794f521dd88a71d8052468349ab
James Rozsypal <jimmerxl@gmail.com>
2023-01-24 09:55:46 -0500

    Add onClick logic for changing the edit button to be the save button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,34 @@ class Header extends Component {
     super();
   }
 
+  toggleEdit() {
+    const editBtn = document.getElementById('btn-edit-save');
+
+    if (editBtn.classList.contains('header-btn-edit')) {
+      editBtn.textContent = 'Save';
+      editBtn.classList.remove('header-btn-edit');
+      editBtn.classList.add('header-btn-save');
+    } else {
+      editBtn.textContent = 'Edit';
+      editBtn.classList.remove('header-btn-save');
+      editBtn.classList.add('header-btn-edit');
+    }
+  }
+
   render() {
     return (
       <header className='page-header'>
         <h1>Curriculum Vitae Builder</h1>
+        <div className='header-input-wrapper'>
+          <button
+            id='btn-edit-save'
+            className='header-btn-edit'
+            onClick={this.toggleEdit}
+          >
+            Edit
+          </button>
+          <button className='header-btn-export'>Export</button>
+        </div>
       </header>
     );
   }
